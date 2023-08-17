b69524ae0b71dde0c5547c0c9c023f4ec4c0ed7a
WanjikuN <cikunjoroge4@gmail.com>
2023-08-17 21:36:55 +0300

    Update components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import Keypad from './components/Keypad';
-import EyesOnMe from './components/EyesOnMe';
+import App from './App';
 
 ReactDOM.render(
-  <div>
-    <Keypad />
-    <EyesOnMe />
-  </div>,
+  
+  <App/>,
   document.getElementById('root')
-);
+  
+)
+
+
+
+
